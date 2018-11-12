08e4cce7729faeb49b71ff51e2b357434a7431f9
per <per@stuffmatic.com>
2018-11-12 17:15:05 +0100

    first pass on build script for creating distribution archives with the version in the file name

--- build.py
+++ build.py
@@ -1,3 +1,39 @@
-# Read version
+import glob
+import os
+import zipfile
 
-# Create zip called fspy-import-[version].zip
+src_dir_name = 'fspy-blender'
+init_file_path = os.path.join(src_dir_name, '__init__.py')
+init_file = open(init_file_path)
+version_parts = []
+for line in init_file.readlines():
+    if '"version":' in line:
+        version_string = line.split("(")[1]
+        version_string = version_string.split(")")[0]
+        version_parts = version_string.split(",")
+        version_parts = map(str.strip, version_parts)
+        break
+init_file.close()
+
+if len(version_parts) == 0:
+    raise "Could not extract version number from " + init_file_path
+
+dist_archive_name = "fSpy-Blender-" + ".".join(version_parts) + ".zip"
+print dist_archive_name
+
+dist_dir_name = "dist"
+
+if not os.path.exists(dist_dir_name):
+    os.makedirs(dist_dir_name)
+
+zipf = zipfile.ZipFile(
+    os.path.join(dist_dir_name, dist_archive_name),
+    'w',
+    zipfile.ZIP_DEFLATED
+)
+
+for py_file in glob.glob(os.path.join(src_dir_name, '*.py')):
+    zipf.write(py_file)
+
+zipf.close()
+
